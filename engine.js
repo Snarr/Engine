e0891b4ec01059ee88e5103b438d3d48d5314cc2
Jacob Snarr <jacobsnarr@gmail.com>
2022-12-22 01:44:43 -0500

    Add comments to document important functions

--- engine.js
+++ engine.js
@@ -1,3 +1,11 @@
+/**
+ * 
+ * @param {CanvasRenderingContext2D} context Document context for World to be drawn upon
+ * @param {number} gravity Acceleration force applied downwards to unanchored Sprites
+ * @param {number} width Width of the world for drawing the background
+ * @param {number} height Height of the world for drawing the background
+ * @returns 
+ */
 function World(context, gravity, width, height) {
   let Background = (color) => {
 
@@ -8,41 +16,67 @@ function World(context, gravity, width, height) {
     return { draw };
   }
 
-  let Sprite = (posX, posY, width, height, color) => {
+  /**
+   * 
+   * @param {number} posX 
+   * @param {number} posY 
+   * @param {number} width 
+   * @param {number} height 
+   * @param {string} color 
+   * @returns
+   */
+  function Sprite (posX, posY, width, height, color) {
     let speedX = 0;
     let speedY = 0;
     let anchored = false;
   
-    let draw = () => {
+    function draw () {
       context.fillStyle = color;
       context.fillRect(posX, posY, width, height)
-    } 
-  
-    let update = () => {
+    }
+    
+    function update () {
       posX += speedX;
 
       posY += speedY;
       if (!anchored) { speedY += gravity };
     }
 
-    let setSpeedX = (x) => {
+    /**
+     * Set the horizontal speed of the Sprite
+     * @param {number} x 
+     */
+    function setSpeedX (x) {
       speedX = x
     }
 
-    let setSpeedY = (y) => {
+    /**
+     * Set the vertical speed of the Sprite
+     * @param {number} y 
+     */
+    function setSpeedY (y) {
       speedY = y
     }
 
-    let setAnchored = (value) => {
+    /**
+     * Set whether or not the object will be affected by other forces (gravity, other Sprites)
+     * @param {Boolean} value 
+     */
+    function setAnchored (value) {
       anchored = value
     }
 
-    let top = () => { return posY };
-    let bottom = () => { return posY + height };
-    let left = () => { return posX };
-    let right = () => { return posX + width };
+    function top () { return posY };
+    function bottom () { return posY + height };
+    function left () { return posX };
+    function right () { return posX + width };
     
-    let collidesWith = (other) => {
+    /**
+     * 
+     * @param {Sprite} other 
+     * @returns {Boolean} True if Sprite is colliding with other Sprite
+     */
+    function collidesWith (other) {
       return (left()-1 < other.right() &&
       right()+1 > other.left() &&
       top()-1 < other.bottom() &&
@@ -54,10 +88,21 @@ function World(context, gravity, width, height) {
   return { Background, Sprite, Input };
 }
 
+
+/**
+ * 
+ * @param {Boolean} debug Enable console logs helpful for debugging
+ * @returns {onKeyPress}
+ */
 function Input (debug) {
   let listeners = {};
-  
-  let onKeyPress = (k, callback) => {
+ 
+  /**
+   * 
+   * @param {string|string[]} k String or array of strings representing key(s) to trigger callback
+   * @param {function} callback Function to run when key(s) are pressed
+   */
+  function onKeyPress (k, callback) {
     if (Array.isArray(k)) {
       for (let i = 0; i < k.length; i++) {
         let key = k[i];
